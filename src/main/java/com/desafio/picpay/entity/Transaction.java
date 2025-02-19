a4a3a199ce6a3a596fb3790e93321b004ab35dbc
shiro-zora <analaura.marques@live.com>
2025-02-18 23:37:07 -0300

    feat: created Transaction entity

---
+++ src/main/java/com/desafio/picpay/entity/Transaction.java
@@ -0,0 +1,69 @@
+package com.desafio.picpay.entity;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import org.apache.catalina.User;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name="payer_id")
+    private User payer;
+
+    @ManyToOne
+    @JoinColumn(name="payee_id")
+    private User payee;
+
+    private LocalDateTime timestamp;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public User getPayer() {
+        return payer;
+    }
+
+    public void setPayer(User payer) {
+        this.payer = payer;
+    }
+
+    public User getPayee() {
+        return payee;
+    }
+
+    public void setPayee(User payee) {
+        this.payee = payee;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
